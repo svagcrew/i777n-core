5886fd58fe4375aa7ad1085ede8cbecdcbd5dafe
Sergei Dmitriev <iserdmi@gmail.com>
2024-04-30 21:58:38 +0400

    X

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { translateWithOpenai } from '@/lib/openai'
+import _ from 'lodash'
+import { deepMap } from 'svag-deep-map'
 import z from 'zod'
 
 export const i777LangsCodes = [
@@ -61,5 +64,201 @@ export const i777LangsCodes = [
   'hy',
   'az',
 ] as const
-
 export const zI777LangCode = z.enum(i777LangsCodes)
+export type I777LangCode = z.infer<typeof zI777LangCode>
+
+export const zI777MetaItem = z.object({
+  srcLang: zI777LangCode,
+  srcValue: z.string(),
+  distLang: zI777LangCode,
+  distValue: z.string().nullable(),
+})
+export const zI777Meta = z.record(zI777MetaItem)
+export type I777MetaItem = z.infer<typeof zI777MetaItem>
+export type I777Meta = Record<string, I777MetaItem>
+export type I777InfoItem = I777MetaItem & {
+  actual: boolean
+  currentSrcValue: string
+}
+export type I777Info = Record<string, I777InfoItem>
+
+export const getClearI777Meta = ({
+  srcLang,
+  distLangs,
+  content,
+}: {
+  srcLang: I777LangCode
+  distLangs: I777LangCode[]
+  content: Record<string, any>
+}) => {
+  const meta: I777Meta = {}
+  for (const lang of distLangs) {
+    deepMap(content, ({ path, value }) => {
+      const valueString = typeof value === 'string' ? value : typeof value === 'number' ? value.toString() : null
+      if (valueString) {
+        meta[`${lang}.${path}`] = {
+          srcLang: srcLang,
+          srcValue: valueString,
+          distLang: lang,
+          distValue: lang === srcLang ? valueString : null,
+        }
+      }
+      return value
+    })
+  }
+  return { meta }
+}
+
+export const normalizeI777Meta = ({
+  metaSource,
+  srcLang,
+  distLangs,
+  content,
+}: {
+  metaSource: I777Meta
+  srcLang: I777LangCode
+  distLangs: I777LangCode[]
+  content: Record<string, any>
+}) => {
+  const metaValid = zI777Meta.parse(metaSource)
+  const { meta: metaNormalized } = getClearI777Meta({ srcLang, distLangs, content })
+  for (const key of Object.keys(metaNormalized)) {
+    if (metaValid[key]) {
+      metaNormalized[key] = metaValid[key]
+    }
+  }
+  return { meta: metaNormalized }
+}
+
+export const getI777Info = ({
+  meta,
+  content,
+  srcLang,
+}: {
+  meta: I777Meta
+  content: Record<string, any>
+  srcLang: I777LangCode
+}) => {
+  const info: I777Info = {}
+  for (const [metaKey, metaValue] of Object.entries(meta)) {
+    const contentPath = metaKey.split('.').slice(1).join('.')
+    const currentSrcValue = _.get(content, contentPath)
+    const actual = metaValue.srcValue === currentSrcValue && !!metaValue.distValue && metaValue.srcLang === srcLang
+    info[metaKey] = {
+      ...meta[metaKey],
+      currentSrcValue,
+      actual,
+    }
+  }
+  return { info }
+}
+
+export const getI777InfoForLang = ({ info, lang }: { info: I777Info; lang: string }) => {
+  return _.pickBy(info, (value) => value.distLang === lang)
+}
+
+export const getFlatI777ContentByI777Meta = ({ meta, lang }: { meta: I777Meta; lang: string }) => {
+  const flatContent: Record<string, string> = {}
+  for (const [key, value] of Object.entries(meta)) {
+    if (value.distLang === lang && value.distValue) {
+      flatContent[key.split('.').slice(1).join('.')] = value.distValue
+    }
+  }
+  return { flatContent }
+}
+
+export const getFlatI777ContentByI777Content = ({ content }: { content: Record<string, any> }) => {
+  const flatContent: Record<string, string> = {}
+  deepMap(content, ({ path, value }) => {
+    const valueString = typeof value === 'string' ? value : typeof value === 'number' ? value.toString() : null
+    if (valueString) {
+      flatContent[path] = valueString
+    }
+    return value
+  })
+  return { flatContent }
+}
+
+export const flatI777ContentToI777Content = ({ flatContent }: { flatContent: Record<string, string> }) => {
+  const content: Record<string, any> = {}
+  for (const [path, value] of Object.entries(flatContent)) {
+    _.set(content, path, value)
+  }
+  return { content }
+}
+
+export const getNotTranslatedKeysByI777Info = ({ info, lang }: { info: I777Info; lang?: I777LangCode }) => {
+  const notTranslatedKeys = Object.entries(info)
+    .filter(([, value]) => {
+      return !value.actual && (!lang || value.distLang === lang)
+    })
+    .map(([keyWithLang]) => keyWithLang.split('.').slice(1).join('.'))
+  return { notTranslatedKeys }
+}
+
+export const getNotTranslatedLangsByI777Info = ({ info }: { info: I777Info }) => {
+  const notTranslatedLangsNotUnique = Object.values(info)
+    .filter((value) => !value.actual)
+    .map((value) => value.distLang)
+  const notTranslatedLangs = Array.from(new Set(notTranslatedLangsNotUnique))
+  return { notTranslatedLangs }
+}
+
+export const isFullyTranslatedByI777Info = ({ info, lang }: { info: I777Info; lang?: I777LangCode }) => {
+  return !getNotTranslatedKeysByI777Info({ info, lang }).notTranslatedKeys.length
+}
+
+export const translate = async ({
+  content,
+  meta,
+  srcLang,
+  distLang,
+  openaiApiKey,
+}: {
+  content: Record<string, any>
+  meta: I777Meta
+  srcLang: I777LangCode
+  distLang: I777LangCode
+  openaiApiKey?: string
+}) => {
+  if (srcLang === distLang) {
+    return { content, meta, wasTranslated: false }
+  }
+  const { info } = getI777Info({ meta, content, srcLang })
+  const { flatContent: flatSrcContent } = getFlatI777ContentByI777Content({ content })
+  const { flatContent: flatDistContent } = getFlatI777ContentByI777Meta({ meta, lang: distLang })
+  const { notTranslatedKeys } = getNotTranslatedKeysByI777Info({ info, lang: distLang })
+  if (Object.keys(flatSrcContent).length === 0) {
+    return { content, meta, wasTranslated: false, message: 'There are no keys in source content' }
+  }
+  if (!notTranslatedKeys.length) {
+    return { content, meta, wasTranslated: false, message: 'There are no keys to translate' }
+  }
+
+  const { updatedFlatDistContent } = await translateWithOpenai({
+    srcLang,
+    distLang,
+    flatSrcContent,
+    flatDistContent,
+    notTranslatedKeys,
+    openaiApiKey,
+  })
+  const { content: distContent } = flatI777ContentToI777Content({
+    flatContent: updatedFlatDistContent,
+  })
+  const distMeta = _.cloneDeep(meta)
+  for (const key of notTranslatedKeys) {
+    const srcValue = flatSrcContent[key]
+    const distValue = updatedFlatDistContent[key]
+    if (!distValue) {
+      continue
+    }
+    distMeta[`${distLang}.${key}`] = {
+      srcLang,
+      srcValue,
+      distLang,
+      distValue,
+    }
+  }
+  return { content: distContent, meta: distMeta, wasTranslated: true }
+}
---
+++ src/lib/openai.ts
@@ -0,0 +1,143 @@
+/* eslint-disable no-console */
+import dedent from 'dedent'
+import OpenAI from 'openai'
+import { encoding_for_model } from 'tiktoken'
+import z from 'zod'
+
+export const getChatGptTextRequestPrice = ({
+  promptTokensCount,
+  completionTokensCount,
+  chatGptModel,
+}: {
+  promptTokensCount: number
+  completionTokensCount: number
+  chatGptModel: 'gpt-4-turbo'
+}) => {
+  const pricePer1kInputTokens = {
+    'gpt-4-turbo': 0.00008,
+  }[chatGptModel]
+  const pricePer1kOutputTokens = {
+    'gpt-4-turbo': 0.00008,
+  }[chatGptModel]
+  return (promptTokensCount * pricePer1kInputTokens + completionTokensCount * pricePer1kOutputTokens) / 1000
+}
+
+export const getChatGptModelTokensLimit = (chatGptModel: 'gpt-4-turbo') => {
+  return {
+    'gpt-4-turbo': 128000,
+  }[chatGptModel]
+}
+
+export const getTokensCount = ({ text, chatGptModel }: { text: string; chatGptModel: 'gpt-4-turbo' }) => {
+  const enc = encoding_for_model(chatGptModel)
+  return enc.encode(text).length
+}
+
+export const translateWithOpenai = async ({
+  srcLang,
+  distLang,
+  flatSrcContent,
+  flatDistContent,
+  notTranslatedKeys,
+  openaiApiKey,
+  verbose = true,
+}: {
+  srcLang: string
+  distLang: string
+  flatSrcContent: Record<string, string>
+  flatDistContent: Record<string, string>
+  notTranslatedKeys: string[]
+  openaiApiKey?: string
+  verbose?: boolean
+}) => {
+  openaiApiKey = openaiApiKey || process?.env?.OPENAI_API_KEY
+  if (!openaiApiKey) {
+    throw new Error('OPENAI_API_KEY is not defined')
+  }
+  const openai = new OpenAI({
+    apiKey: openaiApiKey,
+  })
+  const chatGptModel = 'gpt-4-turbo'
+  const originalContentPart = dedent`
+    New original content, language ${srcLang}:
+    ${Object.entries(flatSrcContent)
+      .map(([key, value]) => `${key}: ${value}`)
+      .join('\n')}
+  `
+  const translatedContentPart = !Object.entries(flatDistContent).length
+    ? null
+    : dedent`
+    Previously translated content, language ${distLang}:
+    ${Object.entries(flatDistContent)
+      .map(([key, value]) => `${key}: ${value}`)
+      .join('\n')}
+  `
+  const taskContentPart = dedent`
+    Give me fresh translations for keys from ${srcLang} language to ${distLang} language:
+    ${notTranslatedKeys.join('\n')}
+  `
+  const requestContent = [originalContentPart, translatedContentPart, taskContentPart].filter(Boolean).join('\n\n')
+  if (verbose) {
+    console.info('Request content:')
+    console.info(requestContent)
+  }
+  const res = await openai.chat.completions.create({
+    model: chatGptModel,
+    messages: [
+      {
+        role: 'system',
+        content: `You are a translater. You should translate the content from ${srcLang} to ${distLang}. Whenever you call any function, you should pass already translated arguments.`,
+      },
+      {
+        role: 'user',
+        content: requestContent,
+      },
+    ],
+    function_call: { name: 'finishTranslation' },
+    functions: [
+      {
+        name: 'finishTranslation',
+        description: `Finish translation the content from ${srcLang} to ${distLang}. This function should be called with already translated content to language ${distLang}.`,
+        parameters: {
+          type: 'object',
+          properties: {
+            ...notTranslatedKeys.reduce(
+              (acc, key) => {
+                acc[key] = { type: 'string' }
+                return acc
+              },
+              {} as Record<string, { type: string }>
+            ),
+          },
+          required: notTranslatedKeys,
+        },
+      },
+    ],
+  })
+  const promptTokensCount = res.usage?.prompt_tokens || 0
+  const completionTokensCount = res.usage?.completion_tokens || 0
+  const price = getChatGptTextRequestPrice({ chatGptModel, promptTokensCount, completionTokensCount })
+  const serializedFnArguments = (res.choices[0].message as any)?.function_call?.arguments
+  const parsedFnArguments = (() => {
+    try {
+      return JSON.parse(serializedFnArguments)
+    } catch (error: any) {
+      throw new Error(`Failed to parse function arguments: ${serializedFnArguments}`)
+    }
+  })()
+  const parsedFnArgumentsValidationResult = z.record(z.string()).safeParse(parsedFnArguments)
+  if (parsedFnArgumentsValidationResult.success === false) {
+    throw new Error(`Function arguments are not valid: ${JSON.stringify(parsedFnArguments)}`)
+  }
+  const safeFnArguments = parsedFnArgumentsValidationResult.data
+  const updatedFlatDistContent: Record<string, string> = {}
+  for (const [key, value] of Object.entries(flatDistContent)) {
+    updatedFlatDistContent[key] = value
+  }
+  for (const [key, value] of Object.entries(safeFnArguments)) {
+    if (notTranslatedKeys.includes(key)) {
+      updatedFlatDistContent[key] = value
+    }
+  }
+  return { updatedFlatDistContent, requestContent, price }
+}
